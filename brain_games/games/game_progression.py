a654beabadd25ec855e24f32f31b92e7a6b3742d
Rem <morrig407@gmail.com>
2024-01-17 19:15:37 +0700

    Change make_progression function to range()

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -5,20 +5,6 @@ DESCRIPTION = "What number is missing in the progression?"
 PROGRESSION_SIZE = 10
 
 
-def get_progression(size: int) -> list[int]:
-    rand_min = 1
-    rand_max = 10
-    start_number = randint(rand_min, rand_max)
-    interval = randint(rand_min, rand_max)
-
-    progression = []
-    temp = start_number
-    for i in range(size):
-        progression.append(temp + i * interval)
-
-    return progression
-
-
 def censor_progression(progression: list[int]) -> (list[int], int):
     rand_idx = randint(0, len(progression) - 1)
     correct_el = progression[rand_idx]
@@ -37,7 +23,13 @@ def get_str_progression(progression: list[int]) -> str:
 
 
 def make_progression() -> (str, int):
-    progression = get_progression(PROGRESSION_SIZE)
+    rand_min = 1
+    rand_max = 10
+    rand_start = randint(rand_min, rand_max)
+    rand_step = randint(rand_min, rand_max)
+    progression = list(range(rand_start,
+                             rand_start + rand_step * PROGRESSION_SIZE,
+                             rand_step))
     censored_progression, correct_el = censor_progression(progression)
     return get_str_progression(censored_progression), correct_el
 
