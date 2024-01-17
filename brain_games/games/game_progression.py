63820fe258e3d153dc56f132c1928223727fba4f
Rem <morrig407@gmail.com>
2024-01-17 19:22:21 +0700

    Remove get_str_progression(), replace with map()

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -12,16 +12,6 @@ def censor_progression(progression: list[int]) -> (list[int], int):
     return progression, correct_el
 
 
-def get_str_progression(progression: list[int]) -> str:
-    line = ""
-    for el in progression:
-        if el is not None:
-            line += str(el) + " "
-        else:
-            line += ".. "
-    return line.strip()
-
-
 def make_progression() -> (str, int):
     rand_min = 1
     rand_max = 10
@@ -31,7 +21,8 @@ def make_progression() -> (str, int):
                              rand_start + rand_step * PROGRESSION_SIZE,
                              rand_step))
     censored_progression, correct_el = censor_progression(progression)
-    return get_str_progression(censored_progression), correct_el
+    line = " ".join(list(map(str, censored_progression))).replace("None", "..")
+    return line, correct_el
 
 
 def get_question_and_answer() -> (str, int):
