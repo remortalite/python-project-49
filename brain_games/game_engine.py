dee15ebe30b8176cc0a68e42c4eb9c03d00096ab
Rem <morrig407@gmail.com>
2024-01-17 22:36:55 +0700

    Refactor logic of game_engine

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -6,33 +6,26 @@ import prompt
 TRY_NUMBER = 3
 
 
-def game_step(game):
-
-    question, answer = game.get_question_and_answer()
-
-    print("Question:", question)
-
-    user_answer = prompt.string("Your answer: ")
-    result = utils.is_answer_correct(user_answer, answer)
-    if result:
-        return True
-    return False
-
-
-def is_win(game) -> bool:
-    for _ in range(TRY_NUMBER):
-        if not game_step(game):
-            return False
-    return True
-
-
 def run(game):
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
 
     print(game.DESCRIPTION)
 
-    result = is_win(game)
+    result = None
+    # run several times
+    for _ in range(TRY_NUMBER):
+
+        question, answer = game.get_question_and_answer()
+
+        print("Question:", question)
+        user_answer = prompt.string("Your answer: ")
+
+        result = utils.is_answer_correct(user_answer, answer)
+
+        if not result:
+            break
+
     if result:
         print(f"Congratulations, {name}!")
     else:
