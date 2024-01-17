c87319bb9446cc7f0eb0391e05fb8d86713cbf73
Rem <morrig407@gmail.com>
2024-01-17 19:37:23 +0700

    Fixed is_prime() for numbers below two

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -5,6 +5,8 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number: int) -> bool:
+    if number <= 1:
+        return False
     for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
             return False
